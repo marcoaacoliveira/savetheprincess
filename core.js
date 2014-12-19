84426d69d354b66332063aba22091deef1dc5cb1
marcoaacoliveira <marcoaacoliveira@gmail.com>
2014-12-19 16:45:20 -0300

    Feito o display pra personagens criados

--- core.js
+++ core.js
@@ -19,20 +19,20 @@ var verifySkill = function (limit) {
     var value_skill;
     do {
         value_skill = calculaCaracteristica();
-    } while (value_skill > limit);
-    return value_skill;
+    } while (value_skill > limit && limit !==0);
+    return Math.max(value_skill, 0);
 };
 
-var create = function (name, limit) {
+var createCharacter = function (name, limit) {
     'use strict';
     var character = {name : name}, value_skill;
     character.forc = 0;
     character.arm = 0;
     character.res = 0;
-    if(limit > 0){
+    if (limit > 0) {
         value_skill = verifySkill(limit);
         limit -= value_skill;
-        character.forc = verifySkill(limit);
+        character.forc = value_skill;
     
     }
     if (limit > 0) {
@@ -49,29 +49,35 @@ var create = function (name, limit) {
     
     character.max_hp = character.res * 5;
     character.current_hp = character.max_hp;
-    character.attack = function(){
+    character.attack = function () {
         return rollDice(character.forc);
     };
-    character.defend = function(){
+    character.defend = function () {
         return rollDice(character.arm);
-    }
+    };
     return character;
+};
+
+var turn = function (attacker, defender) {
+    'use strict';
+    var totalDamage = attacker.attack(), totalArmor =  defender.defend();
+    if (totalDamage > totalArmor) {
+        defender.current_hp = defender.current_hp - (totalDamage - totalArmor);
+    }
+    return defender;
+        
 };
 
 var battle = function (hero_one, hero_two) {
     'use strict';
     var totalDamage = 0, totalArmor = 0;
-    while (hero_one.current_hp >= 0 && hero_two.current_hp >= 0) {
-        totalDamage = hero_one.attack();
-        totalArmor =  hero_two.defend();
-        if (totalDamage > totalArmor) {
-            hero_two.current_hp = hero_two.current_hp - (totalDamage - totalArmor);
+    while (hero_one.current_hp > 0 && hero_two.current_hp > 0) {
+        if (hero_one.current_hp > 0) {
+            hero_two = turn(hero_one, hero_two);
         }
-        totalDamage = hero_two.attack();
-        totalArmor = hero_one.defend();
-        if (totalDamage > totalArmor) {
-            hero_one.current_hp = hero_one.current_hp - (totalDamage - totalArmor);
-        }        
+        if (hero_two.current_hp > 0) {
+            hero_one = turn(hero_two, hero_one);
+        }
     }
     if (hero_one.current_hp <= 0) {
         return hero_two;
@@ -79,11 +85,25 @@ var battle = function (hero_one, hero_two) {
     return hero_one;
 };
 
+var print_character = function (hero) {
+    var display = document.getElementById('display'), no = document.createElement('div'),
+        name = document.createTextNode('name: '+ hero.name +' ');
+        no.appendChild(name);
+        forc = document.createTextNode('force: '+ hero.forc +' ');
+        no.appendChild(forc);
+        resist = document.createTextNode('resist: '+ hero.res+' ');
+        no.appendChild(resist);
+        armor = document.createTextNode("armor: "+ hero.arm+' ');
+        no.appendChild(armor);
+    display.appendChild(no);
+}
+
 var start_battle = function () {
     'use strict';
     var characters = [];
-    characters.push(create("marco", 10));
-    characters.push(create("robsao", 10));
-
+    characters.push(createCharacter('marco', 10));
+    print_character(characters[0]);
+    characters.push(createCharacter('robsao', 10));
+    print_character(characters[1]);
     return battle(characters[0], characters[1]);
-}
+};
